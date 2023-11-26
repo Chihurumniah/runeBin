cefc0a64b346752e646180974c6c0fc3ed91f9c5
miracle <ochihurumnanya@gmail.com>
2023-11-26 04:06:17 -0800

    updated

--- www/js/main.js
+++ www/js/main.js
@@ -1,5 +1,13 @@
 //import { Browser } from "@capacitor/browser";
+import { App } from "@capacitor/app";
 
+App.addListener("backButton", ({ canGoBack }) => {
+  if (canGoBack) {
+    window.history.back();
+  } else {
+    App.exitApp();
+  }
+});
 /**
  * const openCapSite = async () => {
   await Browser.open({ url: "https://www.runebin.com/" });
